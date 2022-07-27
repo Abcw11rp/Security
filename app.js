fc42f4dcca048bc5d6210f626285c6d74ba6088a
vidiant <vidiantip@gmail.com>
2022-07-27 22:29:54 +0700

    add files

---
+++ app.js
@@ -0,0 +1,72 @@
+//jshint esversion:6
+require('dotenv').config()
+const express=require("express");
+const app=express();
+const bodyParser=require("body-parser")
+app.set("view engine", "ejs");
+app.use(express.static('public'));
+const mongoose=require("mongoose")
+app.use(bodyParser.urlencoded({extended: true}));
+const encrypt=require("mongoose-encryption");
+require('dotenv').config()
+const port=3000
+mongoose.connect("mongodb://localhost:27017/udemy",{ useNewUrlParser: true, useUnifiedTopology: true},()=>{
+    console.log("connect")
+  })
+const userSchema=new mongoose.Schema({
+    email:String,
+    password:String
+})
+
+
+userSchema.plugin(encrypt, { secret:process.env.SECRET, encryptedFields: ['password'] })
+const User=new mongoose.model("User",userSchema)
+
+
+app.get("/",(req,res)=>{
+    res.render("home")
+})
+app.get("/login",(req,res)=>{
+    res.render("login")
+})
+app.get("/register",(req,res)=>{
+    res.render("register")
+})
+
+app.post("/register",(req,res)=>{
+    const newUser=new User({
+        email:req.body.username,
+        password:req.body.password
+    })
+    newUser.save((err,result)=>{
+        if (err){
+            console.log(err)
+        }else{
+            res.render("secrets")
+        }
+    })
+})
+
+app.post("/login",(req,res)=>{
+    const username=req.body.username
+    const password=req.body.password
+    User.findOne({email:username},(err,found)=>{
+        if(err){
+            console.log(err)
+        }else{
+            if(found){
+                if(found.password==password){
+                    res.render("secrets")
+                }else{
+                    res.send("wrong")
+                }
+            }else{
+                res.send("not found")
+            }
+        }
+    })
+})
+
+app.listen(port,()=>{
+    console.log(`connect to localhost:${port}`)
+})
